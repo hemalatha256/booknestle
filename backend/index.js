a180ef63fccb06d48e0baa0ede7beb3537f5f7e3
Sashank999 <bandi.rao999@gmail.com>
2025-03-23 12:47:15 +0530

    Setup routers and basic authentication validation.

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,35 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
+import { setAuthenticatedRequestProperty, blockIfNotLoggedIn } from "./middlewares.js";
+import { adminRouter } from "./routers/admin.js";
+import { userRouter } from "./routers/user.js";
+import { bookRouter } from "./routers/book.js";
+import { sellerRouter } from "./routers/seller.js";
 
 const app = express();
 const port = process.env.PORT;
 
+// Global middleware setup.
 app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+// Custom authentication middleware.
+app.use(setAuthenticatedRequestProperty);
 
+// Set up routers.
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+app.use("/seller", sellerRouter);
+
+app.use("/books", blockIfNotLoggedIn);
+app.use("/books", bookRouter);
+
+// Listening.
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-})
-
+	console.log(`Example app listening on port ${port}.`);
+});
